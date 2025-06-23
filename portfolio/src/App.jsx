fb02baf9057cb0a4a973bca84de8a03bfe3a4342
vasiuddin <vasiuddin.mohammed1@gmail.com>
2025-06-23 21:07:25 +0200

    remove extra import app

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import Resume from "./pages/Resume";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
