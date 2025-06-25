57dc491164ff88953374af8ce1409b2a5607a97c
vasiuddin <vasiuddin.mohammed1@gmail.com>
2025-06-25 11:31:35 +0200

    setup tailwindCSS

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div class='flex'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 class="text-3xl font-bold underline text-red-500">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
